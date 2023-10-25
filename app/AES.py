c63378ff6e195e2631328a1812379def116ee87f
Zeerak Zubair <zeerakzubair047@gmail.com>
2023-10-25 18:59:11 +0500

    AES.py updated wrt module Cryptography

--- app/AES.py
+++ app/AES.py
@@ -1,15 +1,29 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import asymmetric
 
 def encrypt_AES(key, plaintext):
-    cipher = AES.new(key, AES.MODE_ECB)
-    ciphertext = cipher.encrypt(plaintext)
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
     return ciphertext
 
 def decrypt_AES(key, ciphertext):
-    cipher = AES.new(key, AES.MODE_ECB)
-    plaintext = cipher.decrypt(ciphertext)
-    return plaintext
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+    return unpadded_data
 
 # Example usage:
 key = get_random_bytes(16)  # 128-bit key
